ca555db64baa561f134aa6d2ed0be572bfe7abf7
chinemerem2525 <chubexchange@gmail.com>
2025-06-23 23:30:15 +0100

    Monday-section5

--- src/app/shared/components/all-shortlets/all-shortlets.component.ts
+++ src/app/shared/components/all-shortlets/all-shortlets.component.ts
@@ -8,22 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class AllShortletsComponent implements OnInit {
 properties = [
   {
-    image: '../../assets/img/product-3/1.jpg',
-    agentImage: '../../assets/img/blog/author.jpg',
-    title: 'New Apartment Nice View',
-    location: 'Belmont Gardens, Chicago',
+    id: '678564',
+    image: '../../assets/img/gallery/10.jpg',
+    title: 'Stylish 2-Bedroom Apartment',
+    location: 'Lekki phase 1',
     beds: 3,
     baths: 2,
+    type: '2 Bed',
+    badges: ['Apartment'],
     squareFt: 3450,
     badge: 'For Rent',
     price: '$34,900',
     priceLabel: '/Month'
   },
   {
-    image: '../../assets/img/product-3/2.jpg',
-    agentImage: '../../assets/img/blog/author.jpg',
-    title: 'New Apartment Nice View',
-    location: 'Belmont Gardens, Chicago',
+    image: '../../assets/img/img-slide/21.jpg',
+    title: 'Luxurious 4-Bedroom Waterfront Duplex',
+    location: 'Lekki phase 1',
     beds: 3,
     baths: 2,
     squareFt: 3450,
@@ -32,10 +33,9 @@ properties = [
     priceLabel: '/Month'
   },
   {
-    image: '../../assets/img/product-3/3.jpg',
-    agentImage: '../../assets/img/blog/author.jpg',
-    title: 'New Apartment Nice View',
-    location: 'Belmont Gardens, Chicago',
+    image: '../../assets/img/img-slide/32.jpg',
+    title: 'Luxurious 2 Bedroom Apartment with Free Wi-Fi',
+    location: 'Ikate, Lekki, Lagos.',
     beds: 3,
     baths: 2,
     squareFt: 3450,
@@ -45,8 +45,7 @@ properties = [
   },
     {
     image: '../../assets/img/product-3/1.jpg',
-    agentImage: '../../assets/img/blog/author.jpg',
-    title: 'New Apartment Nice View',
+    title: '2 Bedroom Apartment With Bq',
     location: 'Belmont Gardens, Chicago',
     beds: 3,
     baths: 2,
