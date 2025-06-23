20fe6b71c9a1d72a2065b1a8d29a317ef712e626
chinemerem2525 <chubexchange@gmail.com>
2025-06-23 16:55:11 +0100

    Monday-section3

--- src/app/shared/components/all-shortlets/all-shortlets.component.ts
+++ src/app/shared/components/all-shortlets/all-shortlets.component.ts
@@ -78,6 +78,42 @@ properties = [
     badge: 'For Rent',
     price: '$34,900',
     priceLabel: '/Month'
+  },
+    {
+    image: '../../assets/img/product-3/1.jpg',
+    agentImage: '../../assets/img/blog/author.jpg',
+    title: 'New Apartment Nice View',
+    location: 'Belmont Gardens, Chicago',
+    beds: 3,
+    baths: 2,
+    squareFt: 3450,
+    badge: 'For Rent',
+    price: '$34,900',
+    priceLabel: '/Month'
+  },
+  {
+    image: '../../assets/img/product-3/2.jpg',
+    agentImage: '../../assets/img/blog/author.jpg',
+    title: 'New Apartment Nice View',
+    location: 'Belmont Gardens, Chicago',
+    beds: 3,
+    baths: 2,
+    squareFt: 3450,
+    badge: 'For Sale',
+    price: '$34,900',
+    priceLabel: '/Month'
+  },
+  {
+    image: '../../assets/img/product-3/3.jpg',
+    agentImage: '../../assets/img/blog/author.jpg',
+    title: 'New Apartment Nice View',
+    location: 'Belmont Gardens, Chicago',
+    beds: 3,
+    baths: 2,
+    squareFt: 3450,
+    badge: 'For Rent',
+    price: '$34,900',
+    priceLabel: '/Month'
   }
 ];
 
