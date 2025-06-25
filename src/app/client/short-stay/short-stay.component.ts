f3cfe0f7fe5729b9d5d6df5e86485e312662b34a
chinemerem2525 <chubexchange@gmail.com>
2025-06-25 22:36:49 +0100

    Wednesday-sectiont4

--- src/app/client/short-stay/short-stay.component.ts
+++ src/app/client/short-stay/short-stay.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShortStayComponent implements OnInit {
 
+     searchData = {
+    location: '',
+    purpose: 'shortlet',
+    type: '',
+    beds: '',
+    minPrice: '',
+    maxPrice: ''
+  };
+
+  prices: number[] = [50000, 100000, 200000, 500000, 1000000, 5000000, 10000000];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  searchProperty() {
+    console.log('Search Filters:', this.searchData);
+    alert('Search triggered! Check console for filters.');
+  }
 }
